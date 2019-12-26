eb14e5bdc5b90a19a4bdd8d3b976cfc164dd99d6
r-kojima <kojima@sailboat.co.jp>
2019-12-26 19:39:26 +0900

    設定ページの新規作成

---
+++ src/main/java/net/phoneportal/idea/fuelphp/FuelSettings.java
@@ -0,0 +1,44 @@
+package net.phoneportal.idea.fuelphp;
+
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.project.Project;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@State(
+        name = "FuelPluginSettings",
+        storages = {
+                @Storage("fuel-plugin.xml")
+        }
+)
+public class FuelSettings implements PersistentStateComponent<FuelSettings> {
+
+    public String mainLanguage;
+
+    public boolean dismissEnableNotification = false;
+
+    public static FuelSettings getInstance(@NotNull Project project) {
+        return ServiceManager.getService(project, FuelSettings.class);
+    }
+
+    public String getMainLanguage() {
+        return !StringUtils.isBlank(mainLanguage) ? mainLanguage : "en";
+    }
+
+    @Nullable
+    @Override
+    public FuelSettings getState() {
+        return this;
+    }
+
+    @Override
+    public void loadState(@NotNull FuelSettings settings) {
+        XmlSerializerUtil.copyBean(settings, this);
+    }
+
+}
---
+++ src/main/java/ui/FuelProjectSettingForm.java
@@ -0,0 +1,70 @@
+package ui;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import net.phoneportal.idea.fuelphp.FuelSettings;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class FuelProjectSettingForm implements Configurable {
+
+    private Project project;
+
+    private JPanel mainPanel;
+    private JTextField textMainLang;
+
+    public FuelProjectSettingForm(@NotNull final Project project) {
+        this.project = project;
+    }
+
+    @Nls(capitalization = Nls.Capitalization.Title)
+    @Override
+    public String getDisplayName() {
+        return "FuelPHP Plugin";
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        return mainPanel;
+    }
+
+    @Override
+    public boolean isModified() {
+        return !textMainLang.getText().equals(getSettings().getMainLanguage());
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+        getSettings().mainLanguage = textMainLang.getText();
+    }
+
+    @Override
+    public void reset() {
+        updateUIFromSettings();
+    }
+
+    @Override
+    public void disposeUIResources() {
+
+    }
+
+    private void updateUIFromSettings() {
+        textMainLang.setText(getSettings().getMainLanguage());
+    }
+
+    private FuelSettings getSettings() {
+        return FuelSettings.getInstance(this.project);
+    }
+
+}
